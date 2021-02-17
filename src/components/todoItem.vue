b4d7806393e2d415c805c232e81ef052e8915ba2
illia2021 <ilya.zyskunov@gmail.com>
2021-02-17 11:38:39 +0200

    new

--- src/components/todoItem.vue
+++ src/components/todoItem.vue
@@ -1,6 +1,11 @@
 <template>
     <li>
-        {{todo.title}}
+        <span>
+            <input type="checkbox">
+            <strong>{{todo.id}}</strong>
+            {{todo.title}}
+        </span>
+        <button>&times;</button>
     </li>
 </template>
 
@@ -14,3 +19,13 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    li {
+        border: 1px solid #ccc;
+        display: flex;
+        justify-content: space-between;
+        padding: .5rem 2rem;
+        margin-bottom: 1rem;
+    }
+</style>
